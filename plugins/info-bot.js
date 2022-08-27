c8597770c082ddcb5e2bfd839850bb441d1966af
Matias <109123781+ElMatias-PY@users.noreply.github.com>
2022-08-27 14:52:59 -0400

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,15 +1,27 @@
-import fs from "fs"
-let handler = m => m
-handler.all = async function (m) {
-let vn = './media/bot.mp3'
-let chat = global.db.data.chats[m.chat]
+import util from 'util'
+import path from 'path'
+import fs from 'fs'
+let handler = async (m, { conn}) => {
+await conn.sendMessage(m.chat, { react: { text: '✨', key: m.key } })
+let pp = './Menu2.jpg'
+let name = conn.getName(m.sender)
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
-message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}  
-if (/^bot$/i.test(m.text) && !chat.isBanned) { 
-conn.sendPresenceUpdate('recording', m.chat)    
-conn.sendButton(m.chat, '*𝙷𝙾𝙻𝙰, ¿𝙲𝙾𝙼𝙾 𝚃𝙴 𝙿𝚄𝙴𝙳𝙾 𝙰𝚈𝚄𝙳𝙰𝚁?*', wm, [['𝙼𝙴𝙽𝚄 𝙳𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
-conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: estiloaudio })}
-return !0
+message: { 
+orderMessage: { itemCount : 69, status: 1, surface : 1, message: '𝐓𝐡𝐞 𝐊𝐢𝐧𝐠 - 𝐁𝐨𝐭', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'    
+}}}
+let fgac = [ pp, "adri"]
+let nfg = fgac[Math.floor(Math.random() * fgac.length)] 
+
+conn.sendHydrated(m.chat, `\n𝙷𝙾𝙻𝙰 *${name}* 𝙿𝚄𝚃𝙰 \n\n*𝙴𝙽 𝚀𝚄𝙴 𝚃𝙴 𝙿𝚄𝙴𝙳𝙾 𝙰𝚈𝚄𝙳𝙰𝚁?*`, wm, null, 'https://instagram.com/matiass.zzz', 'Sigueme en Instagram', null, null, [ 
+      ['⦙☰ 𝙼𝙴𝙽𝚄', '.help'],
+      ['⦙☰ 𝙲𝚁𝙴𝙰𝙳𝙾𝚁', '.owner'],
+      ['⦙☰ 𝙳𝙾𝙽𝙰𝚁', '.donar'],
+    ], m)
+// conn.sendFile(m.chat, null, null, m, null, true, {
+// type: 'audioMessage', 
+// ptt: true
+// })
 }
+handler.customPrefix = /^(bot|robot)$/i
+handler.command = new RegExp
 export default handler
