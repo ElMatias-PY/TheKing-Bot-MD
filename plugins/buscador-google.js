3317ec96fc414118e45da06f5325e674de9ba3e2
Matias <109123781+ElMatias-PY@users.noreply.github.com>
2022-09-11 01:28:12 -0400

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -24,4 +24,6 @@ m.reply(msg)
 handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
 handler.command = /^googlef?$/i
+handler.exp = 10
+handler.limit = 1
 export default handler
