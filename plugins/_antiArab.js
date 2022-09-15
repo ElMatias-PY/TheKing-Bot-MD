cee5fcb277d85af64eeb91961dc244f56558194d
Matias <109123781+ElMatias-PY@users.noreply.github.com>
2022-09-15 02:20:40 -0400

    Update _antiArab.js

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -10,13 +10,23 @@ if (isBotAdmin && chat.antiArab) {
 		
 if (m.sender.startsWith('212' || '212')) {
 global.db.data.users[m.sender].banned = true
-m.reply(`❎ Ey, Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`❎ Ey, Anti fakes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
    
 if (m.sender.startsWith('234' || '234')) {
 global.db.data.users[m.sender].banned = true
-m.reply(`❎ Ey, Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`❎ Ey, Anti fakes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-   
+
+if (m.sender.startsWith('1' || '1')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`❎ Ey, Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+ 
+if (m.sender.startsWith('91' || '91')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`❎ Ey, Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}  
+ 
 }}
 export default handler
