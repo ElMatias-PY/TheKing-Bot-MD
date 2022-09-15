0de3c1bfaeba78f8c1157181c4ff1d27fa39b124
Matias <109123781+ElMatias-PY@users.noreply.github.com>
2022-09-15 02:49:43 -0400

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -19,4 +19,5 @@ await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚂
 handler.help = ['join [chat.whatsapp.com]']
 handler.tags = ['premium']
 handler.command = /^join|nuevogrupo$/i
+handler.register = true
 export default handler
