0f1bc8bc3e4c3b273901ee1daee477054153440f
Matias <109123781+ElMatias-PY@users.noreply.github.com>
2022-09-15 02:51:57 -0400

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -24,4 +24,5 @@ console.log(e)
 handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
+handler.register = true
 export default handler
